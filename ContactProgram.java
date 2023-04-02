a2d500a2924402b91bb143a26f6eaa3c552c17ec
justinwang <justinwang407@gmail.com>
2023-04-02 13:21:14 -0400

    java connection file

---
+++ ContactProgram.java
@@ -0,0 +1,45 @@
+package com.db.jsp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class ContactProgram {
+	static String jdbcURL = "jdbc:postgresql://localhost:5432/hotelmanagement";
+    static String username = "justinwang";
+    static String password = "postwan07";
+
+    public static String add() throws SQLException{
+        String address = "";
+
+        Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+        System.out.println("connected");
+        
+        String sql = "SELECT * FROM hotelchain";
+        
+        Statement statement = connection.createStatement();
+        
+        ResultSet result = statement.executeQuery(sql);
+        while (result.next()) {
+        	 address = result.getString("centralOfficeAddress");
+        }
+        
+        connection.close();
+        
+        return address;
+    }
+    
+    public static ResultSet showAll() throws SQLException{
+    	Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+    	String sql = "SELECT * FROM hotelchain";
+        
+        Statement statement = connection.createStatement();
+        
+        ResultSet result = statement.executeQuery(sql);
+        
+		return result;
+    }
+}
